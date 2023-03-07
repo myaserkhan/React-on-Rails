d309f59972f8c718c5178ea9456774cd5b4d5f79
Yasir Khan <khanyaser007@gmail.com>
2023-03-07 19:32:11 +0500

    Adjust components and add store provider

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Greeting from './Greeting';
 
 function App() {
-  return (<Greeting />);
+  return (
+    <div className="App">
+      <h2>Someone says</h2>
+      <Greeting />
+    </div>
+  );
 }
 
 export default App;
